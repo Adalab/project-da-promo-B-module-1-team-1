3f1262d6bc9155433154c93d2e970554ffc069bb
Yadira Gonzalez Lara <larayadi28@gmail.com>
2022-07-13 15:31:11 +0200

    Creación de la BBDD y de la tabla madre SQL

---
+++ proyecto_1_adalab.sql
@@ -0,0 +1,26 @@
+CREATE SCHEMA proyecto_1_adalab;
+
+USE proyecto_1_adalab;
+
+#CReación de la tabla maadre del proyecto
+CREATE TABLE IF NOT EXISTS data_sql(
+index_sql INT NOT NULL,
+q10_part_1 TEXT,
+q10_part_2 TEXT,
+q10_part_3 TEXT,
+q10_part_4 TEXT,
+q10_part_5 TEXT,
+q10_part_6 TEXT,
+q10_part_7 TEXT,
+q10_part_8 TEXT,
+q10_part_9 TEXT,
+q10_part_10 TEXT,
+q10_part_11 TEXT,
+q10_part_12 TEXT,
+q10_part_13 TEXT,
+q10_part_14 TEXT,
+q10_part_15 TEXT,
+q10_part_16 TEXT,
+q10_other TEXT,
+PRIMARY KEY (index_sql));
+
