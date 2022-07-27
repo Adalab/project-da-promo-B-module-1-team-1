7917903a8c29a39a10519c8491fb05ba22bb5c36
NataliaMCatalan <natmarca@gmail.com>
2022-07-27 15:24:48 +0200

    modificacion de ERROR a NULL

---
+++ sql/limpieza_tabla_sql.sql
@@ -0,0 +1,86 @@
+-- En este archivo modificamos el ERROR por NULL --
+
+USE proyecto_1_adalab;
+
+UPDATE data_sql
+SET index_sql= NULL
+WHERE index_sql= "ERROR";
+
+UPDATE data_sql
+SET q10_part_1= NULL
+WHERE q10_part_1= "ERROR";
+
+UPDATE data_sql
+SET q10_part_2= NULL
+WHERE q10_part_2= "ERROR";
+
+UPDATE data_sql
+SET q10_part_3= NULL
+WHERE q10_part_3= "ERROR";
+
+UPDATE data_sql
+SET q10_part_4= NULL
+WHERE q10_part_4= "ERROR";
+
+UPDATE data_sql
+SET q10_part_5= NULL
+WHERE q10_part_5= "ERROR";
+
+UPDATE data_sql
+SET q10_part_6= NULL
+WHERE q10_part_6= "ERROR";
+
+UPDATE data_sql
+SET q10_part_7= NULL
+WHERE q10_part_7= "ERROR";
+
+UPDATE data_sql
+SET q10_part_8= NULL
+WHERE q10_part_8= "ERROR";
+
+UPDATE data_sql
+SET q10_part_9= NULL
+WHERE q10_part_9= "ERROR";
+
+UPDATE data_sql
+SET q10_part_10= NULL
+WHERE q10_part_10= "ERROR";
+
+UPDATE data_sql
+SET q10_part_11= NULL
+WHERE q10_part_11= "ERROR";
+
+UPDATE data_sql
+SET q10_part_12= NULL
+WHERE q10_part_12= "ERROR";
+
+UPDATE data_sql
+SET q10_part_13= NULL
+WHERE q10_part_13= "ERROR";
+
+UPDATE data_sql
+SET q10_part_14= NULL
+WHERE q10_part_14= "ERROR";
+
+UPDATE data_sql
+SET q10_part_15= NULL
+WHERE q10_part_15= "ERROR";
+
+UPDATE data_sql
+SET q10_part_16= NULL
+WHERE q10_part_16= "ERROR";
+
+UPDATE data_sql
+SET q10_other= NULL
+WHERE q10_other= "ERROR";
+
+UPDATE data_sql
+SET d482xta= NULL
+WHERE d482xta= "ERROR";
+
+
+
+
+
+
+
